bc2704935e5aa0f92f2084a44ce37ab835164c24
Tiancheng <yukino161043261@gmail.com>
2025-04-05 21:10:20 +0800

    chore: Regular code maintenance

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,9 +2,42 @@
 // Created by user on 2025/03/02.
 //
 
-#include "main.hpp"
-
 #include <iostream>
+#include <vector>
+#include <functional>
+#include <format>
+
 using namespace std;
 
-int main() { cout << "Hello world" << endl; }
+class Solution {
+public:
+  vector<int> searchRange(vector<int>& nums, int target) {
+    function<int(int)> lowerBound = [&](auto lowerTaget) -> auto {
+      auto left = 0;
+      auto right = static_cast<int>(nums.size());
+      while (left < right) {
+        auto mid = left + (right - left) / 2;
+        if (nums[mid] >= lowerTaget) {
+          right = mid;
+        }
+        else {
+          left = mid + 1;
+        };
+        return left;
+      };
+      };
+
+    auto start = lowerBound(target);
+    if (start == nums.size() || nums[start] != target) {
+      return { -1, -1 };
+    };
+    return { start, lowerBound(target + 1) - 1 };
+  };
+};
+
+int main() {
+  auto testcase = Solution{};
+  auto vec = vector<int>{ 5,7,7,8,8,10 };
+  auto ret = testcase.searchRange(vec, 8);
+  cout << ret[0] << ret[1] << endl;
+}
--- cpp/main.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by user on 2025/03/02.
-//
-
-#ifndef MAIN_H
-#define MAIN_H
-
-#endif  // MAIN_H
