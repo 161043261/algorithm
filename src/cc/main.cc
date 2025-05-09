7afdfd799ee6de8dab956cf7e48bee0b3d34c137
Tiancheng <yukino161043261@gmail.com>
2025-05-09 22:11:52 +0800

    chore: Regular code maintenance

--- src/cc/main.cc
+++ src/cc/main.cc
@@ -11,6 +11,7 @@
 #include <format>
 #include <functional>
 #include <iostream>
+#include <iterator>
 #include <memory>
 #include <numeric>
 #include <queue>
@@ -24,54 +25,8 @@
 using namespace std;
 
 class Solution {
- public:
-  int myAtoi(string s) {
-    auto b = s.begin();
-    // cout << *(++b);
-    // cout << *(--b);
-    for (; b != s.end() && *b == ' '; b++);
+public:
+  string addBinary(string a, string b) {
 
-    auto isNeg = false;
-    if (b + 1 != s.end() && (*b == '-' || *b == '+')) {
-      isNeg = (*b == '-');
-      b++;
-    }
-
-    for (; b != s.end() && *b == '0'; b++);
-    auto e = b;
-    for (; *e >= '0' && *e <= '9' && e != s.end(); e++);
-    auto payload = string(b, e);
-    cout << payload << endl;
-    auto ans = 0;
-    for (auto i = 0; i < payload.size(); i++) {
-      auto j = payload.size() - 1 - i;
-      auto delta = (payload[i] - '0') * pow(10, j);
-      if (delta > INT_MAX - ans) {
-        return isNeg ? INT_MIN : INT_MAX;
-      } else {
-        ans += delta;
-      }
-    }
-    return isNeg ? 0 - ans : ans;
   }
-
-  string intToRoman(int num) {
-    vector<vector<string>> hashArr = {
-        {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
-        {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
-        {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
-        {"", "M", "MM", "MMM"}};
-    string roman = "";
-    roman.append(hashArr[3][num / 1000]);
-    roman.append(hashArr[2][num / 100 % 10]);
-    roman.append(hashArr[1][num / 10 % 10]);
-    roman.append(hashArr[0][num % 10]);
-    return roman;
-  };
 };
-
-int main() {
-  auto _ = Solution{};
-  const auto ret = _.myAtoi("-91283472332");
-  cout << ret << endl;
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
-console.log('index.js')
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
 
-export default {}
+};
