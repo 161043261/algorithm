129249d16b599e50d5dba279a35e4655cc51335e
Tiancheng <yukino161043261@gmail.com>
2025-05-25 16:40:33 +0800

    chore: Regular code maintenance

--- src/js/index.js
+++ src/js/index.js
@@ -1,87 +1,24 @@
 /**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {string[]} words
+ * @return {number}
  */
-var sortColors = function (nums) {
-  /**
-   *
-   * @param {number[]} heap
-   * @param {number} heapSize
-   */
-  const buildMinHeap = (heap, heapSize) => {
-    const lastLeafIdx = heapSize - 1;
-    const lastNotLeafIdx = Math.floor((lastLeafIdx - 1) / 2);
-    for (let idx = lastNotLeafIdx; idx >= 0; idx--) {
-      minHeapify(heap, heapSize, idx);
-    }
-  };
-
-  /**
-   *
-   * @param {number[]} heap
-   * @param {number} heapSize
-   */
-  const buildMaxHeap = (heap, heapSize) => {
-    const lastLeafIdx = heapSize - 1;
-    const lastNotLeafIdx = Math.floor((lastLeafIdx - 1) / 2);
-    for (let idx = lastNotLeafIdx; idx >= 0; idx--) {
-      maxHeapify(heap, heapSize, idx);
-    }
-  };
-
-  /**
-   *
-   * @param {number[]} heap
-   * @param {number} heapSize
-   * @param {number} idx
-   */
-  const minHeapify = (heap, heapSize, idx) => {
-    let childIdx = idx;
-    const leftChildIdx = idx * 2 + 1;
-    const rightChildIdx = leftChildIdx + 1;
-    if (leftChildIdx < heapSize && heap[leftChildIdx] < heap[childIdx]) {
-      childIdx = leftChildIdx;
-    }
-    if (rightChildIdx < heapSize && heap[rightChildIdx] < heap[childIdx]) {
-      childIdx = rightChildIdx;
-    }
-    if (childIdx === idx) {
-      return;
-    }
-    [heap[idx], heap[childIdx]] = [heap[childIdx], heap[idx]];
-    minHeapify(heap, heapSize, childIdx);
-  };
-
-  /**
-   *
-   * @param {number[]} heap
-   * @param {number} heapSize
-   * @param {number} idx
-   */
-  const maxHeapify = (heap, heapSize, idx) => {
-    let childIdx = idx;
-    const leftChildIdx = idx * 2 + 1;
-    const rightChildIdx = leftChildIdx + 1;
-    if (leftChildIdx < heapSize && heap[leftChildIdx] > heap[childIdx]) {
-      childIdx = leftChildIdx;
-    }
-    if (rightChildIdx < heapSize && heap[rightChildIdx] > heap[childIdx]) {
-      childIdx = rightChildIdx;
-    }
-    if (childIdx === idx) {
-      return;
-    }
-    [heap[idx], heap[childIdx]] = [heap[childIdx], heap[idx]];
-    maxHeapify(heap, heapSize, childIdx);
-  };
-
-  let heapSize = nums.length;
-  buildMaxHeap(nums, heapSize);
-  for (; heapSize > 0; heapSize--) {
-    [nums[0], nums[heapSize - 1]] = [nums[heapSize - 1], nums[0]];
-    maxHeapify(nums, heapSize - 1, 0);
+var longestPalindrome = function (words) {
+  const cnt = Array.from({ length: 26 }, () => new Array(26).fill(0));
+  for (const word of words) {
+    cnt[word.charCodeAt(0) - "a".charCodeAt(0)][
+      word.charCodeAt(1) - "a".charCodeAt(0)
+    ]++;
   }
-  return nums;
-};
 
-sortColors([2, 0, 2, 1, 1, 0]);
+  let ans = 0;
+  let odd = 0;
+  for (let i = 0; i < 26; i++) {
+    const c = cnt[i][i];
+    ans += c - c % 2;
+    odd |= c % 2;
+    for (let j = i + 1; j < 26; j++) {
+      ans += Math.min(cnt[i][j], cnt[j][i]) * 2
+    }
+  }
+  return (ans + odd) * 2
+};
