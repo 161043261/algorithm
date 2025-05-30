8a3fff1592cc148f2244727f7d209530fc342829
Tiancheng <yukino161043261@gmail.com>
2025-05-30 23:23:14 +0800

    feat: Introduce new feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,79 @@
 /**
- * @param {string[]} words
- * @return {number}
+ * @param {number[][]} edges1
+ * @param {number[][]} edges2
+ * @param {number} k 边数小于等于 k
+ * @return {number[]}
  */
-var longestPalindrome = function (words) {
-  const cnt = Array.from({ length: 26 }, () => new Array(26).fill(0));
-  for (const word of words) {
-    cnt[word.charCodeAt(0) - "a".charCodeAt(0)][
-      word.charCodeAt(1) - "a".charCodeAt(0)
-    ]++;
+var maxTargetNodes = function (edges1, edges2, k) {
+  if (k <= 0) {
+    return new Array(edges1.length).fill(1);
   }
 
-  let ans = 0;
-  let odd = 0;
-  for (let i = 0; i < 26; i++) {
-    const c = cnt[i][i];
-    ans += c - c % 2;
-    odd |= c % 2;
-    for (let j = i + 1; j < 26; j++) {
-      ans += Math.min(cnt[i][j], cnt[j][i]) * 2
+  /**
+   *
+   * @param {number[][]} edges
+   * @return {number[][]}
+   */
+  const buildTree = (edges) => {
+    // const tree = new Array(edges.length).fill([]);
+    const tree = Array.from({ length: edges.length }, () => []);
+    for (const [curr, next] of edges) {
+      tree[curr].push(next);
+      tree[next].push(curr);
     }
+    return tree;
+  };
+
+  const tree1 = buildTree(edges1);
+  const tree2 = buildTree(edges2);
+
+  /**
+   *
+   * @param {number} curr
+   * @param { number} parent
+   * @param {number} depth
+   * @param {number[][]} tree
+   * @returns
+   */
+  const dfs = (curr, parent, depth, tree) => {
+    if (depth > k) {
+      return 0;
+    }
+    let ret = 1;
+    for (const next of tree[curr]) {
+      if (next !== parent) {
+        ret += dfs(next, curr, depth + 1, tree);
+      }
+    }
+    return ret;
+  };
+
+  let max2 = 0;
+  for (let i = 0; i < edges2.length; i++) {
+    max2 = Math.max(max2, dfs(i, -1, 0, tree2, k - 1));
   }
-  return (ans + odd) * 2
+  const ans = new Array(edges1.length).fill(0);
+  for (let i = 0; i < tree1.length; i++) {
+    ans[i] = dfs(i, -1, 0, tree1, k) + max2;
+  }
+  return ans;
 };
+
+maxTargetNodes(
+  [
+    [0, 1],
+    [0, 2],
+    [2, 3],
+    [2, 4],
+  ],
+  [
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [2, 7],
+    [1, 4],
+    [4, 5],
+    [4, 6],
+  ],
+  2,
+);
