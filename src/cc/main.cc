4bfb2b223478ccd9d616b8d58d06576d006c4ded
Tiancheng <yukino161043261@gmail.com>
2025-05-31 23:53:57 +0800

    feat: Introduce new feature

--- src/cc/main.cc
+++ src/cc/main.cc
@@ -7,6 +7,7 @@
 #include <climits>
 #include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <deque>
 #include <format>
 #include <functional>
@@ -24,9 +25,40 @@
 
 using namespace std;
 
-class Solution {
-public:
-  string addBinary(string a, string b) {
-
+int
+snakesAndLadders(vector<vector<int>>& board) {
+  auto n = static_cast<int>(board.size());
+  vector<int8_t> visited(n * n + 1);
+  visited[1] = true;
+  vector<int> q{1};
+  for (auto step = 0; !q.empty(); step++) {
+    auto temp = q;
+    q.clear();
+    for (auto x : temp) {
+      if (x == n * n) {
+        return step;
+      }
+      for (auto y = x + 1; y <= min(x + 6, n * n); y++) {
+        auto r = (y - 1) / n;
+        auto c = (y - 1) % n;
+        if (r % 2 /** == 1 */) {
+          c = n - 1 - c;
+        }
+        auto next = board[n - 1 - r][c];
+        if (next < 0) {
+          next = y;
+        }
+        if (!visited[next]) {
+          visited[next] = true;
+          q.emplace_back(next);
+        }
+      }
+    }
   }
+  return -1;
+}
+
+class Solution {
+ public:
+  int snakesAndLadders(vector<vector<int>>& board) { return snakesAndLadders(board); }
 };
--- src/main.sh
+++
@@ -1,1 +0,0 @@
-cat ./word.txt | tr -s ' ' '\n' | sort | uniq -c | sort -r | awk '{ print $2, $1 }'
