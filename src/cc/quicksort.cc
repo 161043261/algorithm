8513b1bbb47532099de1f718c60308809d5ae710
hangtiancheng <hangtiancheng@bytedance.com>
2025-06-23 01:31:05 +0800

    feat: Introduce new features

--- src/cc/quicksort.cc
+++ src/cc/quicksort.cc
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void quickSort(vector<int> &nums, const int left, const int right) {
+void quicksort(vector<int> &nums, const int left, const int right) {
   if (left >= right) {
     return;
   }
@@ -28,13 +28,13 @@ void quickSort(vector<int> &nums, const int left, const int right) {
   };
 
   auto divider = partition();
-  quickSort(nums, left, divider - 1);
-  quickSort(nums, divider + 1, right);
+  quicksort(nums, left, divider - 1);
+  quicksort(nums, divider + 1, right);
 }
 
 int main() {
   auto nums = vector<int>{3, 4, 1, 5, 2};
-  quickSort(nums, 0, nums.size() - 1);
+  quicksort(nums, 0, nums.size() - 1);
   for_each(nums.begin(), nums.end(),
            [](const auto &item) { cout << format("{} ", item); });
 }
