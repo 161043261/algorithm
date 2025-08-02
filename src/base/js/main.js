866bd2a7414bc04a1c70737bdf4b40775230285a
Tiancheng <yukino161043261@gmail.com>
2025-08-02 23:17:38 +0800

    feat: Introduce new features

--- src/base/js/main.js
+++ src/base/js/main.js
@@ -1,30 +1,30 @@
-class TimeLimitedCache {
-  /** @type {Map<number, [number, number]>} */
-  kvs = new Map();
+// class TimeLimitedCache {
+//   /** @type {Map<number, [number, number]>} */
+//   kvs = new Map();
 
-  constructor() {}
-  /**
-   * @param {number} key
-   * @param {number} value
-   * @param {number} duration time until expiration in ms
-   * @return {boolean} if un-expired key already existed
-   */
-  set(key, value, duration) {
-    return true;
-  }
+//   constructor() {}
+//   /**
+//    * @param {number} key
+//    * @param {number} value
+//    * @param {number} duration time until expiration in ms
+//    * @return {boolean} if un-expired key already existed
+//    */
+//   set(key, value, duration) {
+//     return true;
+//   }
 
-  /**
-   * @param {number} key
-   * @return {number} value associated with key
-   */
-  get(key) {
-    return 1;
-  }
+//   /**
+//    * @param {number} key
+//    * @return {number} value associated with key
+//    */
+//   get(key) {
+//     return 1;
+//   }
 
-  /**
-   * @return {number} count of non-expired keys
-   */
-  count() {
-    return 1;
-  }
-}
+//   /**
+//    * @return {number} count of non-expired keys
+//    */
+//   count() {
+//     return 1;
+//   }
+// }
