601df62f3bcb83f1054d19e7a92a3c0f51f6aa8b
Tiancheng <yukino161043261@gmail.com>
2025-09-26 09:36:25 +0800

    feat: Introduce new features

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,59 @@
-#include <set>
-#include <string>
-#include <tuple>
-#include <unordered_map>
+#include <algorithm>
+#include <functional>
 #include <vector>
 
 using namespace std;
 
-class MovieRentingSystem {
- private:
-  // shopMovie => price
-  unordered_map<string, int> shopMovie2price;
-  // unrentedMovie => (price, shop)
-  unordered_map<int, set<pair<int, int>>> unrentedMovie2priceShop;
-  // (price, shop, rentedMovie)
-  set<tuple<int, int, int>> rentedMovies;
+namespace ranges_ {
+void sort(vector<int>& nums) {
+  std::sort(nums.begin(), nums.end(),
+            [&](auto a, auto b) -> bool { return a <= b; });
+};
+}  // namespace ranges_
 
-  string getShopMovieKey(int shop, int movie) {
-    return to_string(shop) + ',' + to_string(movie);
-  }
-
+class Solution {
  public:
-  MovieRentingSystem(int n, vector<vector<int>>& entries) {
-    for (auto& e : entries) {
-      auto shop = e[0], movie = e[1], price = e[2];
-      auto shopMovieKey = this->getShopMovieKey(shop, movie);
-      this->shopMovie2price[shopMovieKey] = price;
-
-      if (this->unrentedMovie2priceShop.find(movie) ==
-          this->unrentedMovie2priceShop.cend()) {
-        this->unrentedMovie2priceShop[movie] =
-            set<pair<int, int>>{pair<int, int>{price, shop}};
-      } else {
-        this->unrentedMovie2priceShop[movie].emplace(
-            pair<int, int>{price, shop});
+  int triangleNumber2(vector<int>& nums) {
+    if (nums.size() < 3) {
+      return 0;
+    }
+    // sort(nums.begin(), nums.end(),
+    //      [&](auto a, auto b) -> bool { return a <= b; });
+    ranges_::sort(nums);
+    auto ans = 0;
+    for (auto k = 2; k < nums.size(); k++) {
+      auto i = 0;
+      auto j = k - 1;
+      while (i < j) {
+        if (nums[i] != 0 && nums[i] + nums[j] > nums[k]) {
+          ans += j - i;
+          j--;
+        } else {
+          i++;
+        }
       }
     }
-  }
 
-  vector<int> search(int movie) {
-    auto ans = vector<int>{};
-
-    auto it = unrentedMovie2priceShop.find(movie);
-    if (it == unrentedMovie2priceShop.cend()) {
-      return ans;
-    }
-
-    for (auto& [_, shop] : it->second) {
-      // ans.push_back(shop);
-      ans.emplace_back(shop);
-      if (ans.size() == 5) {
-        break;
-      }
-    }
     return ans;
   }
 
-  void rent(int shop, int movie) {
-    auto shopMovieKey = this->getShopMovieKey(shop, movie);
-    auto price = this->shopMovie2price[shopMovieKey];
-    this->unrentedMovie2priceShop[movie].erase({price, shop});
-    this->rentedMovies.emplace(tuple<int, int, int>{price, shop, movie});
-  }
-
-  void drop(int shop, int movie) {
-    auto shopMovieKey = this->getShopMovieKey(shop, movie);
-    auto price = shopMovie2price[shopMovieKey];
-    this->rentedMovies.erase(tuple<int, int, int>{price, shop, movie});
-    this->unrentedMovie2priceShop[movie].emplace(pair<int, int>{price, shop});
-  }
-
-  vector<vector<int>> report() {
-    auto ans = vector<vector<int>>{};
-    for (auto& [_, shop, movie] : rentedMovies) {
-      ans.emplace_back(vector<int>{shop, movie});
-      if (ans.size() == 5) {
-        break;
+  int triangleNumber(vector<int>& nums) {
+    if (nums.size() < 3) {
+      return 0;
+    }
+    // sort(nums.begin(), nums.end(), [&](auto a, auto b) { return a <= b; });
+    ranges_::sort(nums);
+    auto ans = 0;
+    for (auto i = 0; i < nums.size() - 2; i++) {
+      if (nums[i] == 0) {
+        continue;
+      }
+      auto j = i + 1;
+      for (auto k = i + 2; k < nums.size(); k++) {
+        while (nums[i] + nums[j] <= nums[k]) {
+          j++;
+        }
+        ans += k - j;
       }
     }
     return ans;
