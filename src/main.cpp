13707e1990f562732b19aa2cf78fd7115a041f22
tianchenghang <161043261@qq.com>
2025-11-01 17:35:08 +0800

    feat: Introduce new features

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <unordered_set>
+#include <vector>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
-}
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+    auto numSet = unordered_set<int>(nums.begin(), nums.end());
+    auto root = ListNode{0, head};
+    auto pre = &root;
+    auto cur = root.next;
+    while (cur != nullptr) {
+      if (numSet.contains(cur->val)) {
+        pre->next = cur->next;
+        cur = pre->next;
+      } else {
+        pre = cur;
+        cur = cur->next;
+      }
+    }
+    return root.next;
+  }
+};
