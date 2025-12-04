a483bee91a57337abe55392fd836d40f0ca0a2f0
larkhang <larkhang@tencent.com>
2025-12-04 14:34:47 +0800

    feat: Introduce new features

--- src/solution.cc
+++ src/solution.cc
@@ -1,21 +1,83 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <stack>
+#include <string>
 
 using namespace std;
 
+struct ChrCnt {
+  char chr;
+  int cnt;
+};
+
 class Solution {
  public:
-  int minimumOperations(vector<int>& nums) {
+  int countCollisions(string directions) {
+    auto stk = stack<ChrCnt>{};
     auto ans = 0;
-    for (auto& num : nums) {
-      auto sum = 0;
-      while (num > 0) {
-        sum += num % 10;
-        num /= 10;
+    for (auto chr : directions) {
+      if (stk.size() == 0) {
+        stk.push(ChrCnt{chr, 1});
+        continue;
       }
-      if (sum % 3 != 0) {
-        ans += 1;
+      auto top = stk.top();
+      auto topChr = top.chr;
+      auto topCnt = top.cnt;
+      switch (topChr) {
+        case 'L': {  // topCnt
+          switch (chr) {
+            case 'L': {
+              stk.pop();
+              stk.push(ChrCnt{'L', topCnt + 1});
+              break;
+            }
+            case 'R': {
+              stk.push(ChrCnt{'R', 1});
+              break;
+            }
+            case 'S': {
+              stk.push(ChrCnt{'S', 0});
+              break;
+            }
+          }
+          break;
+        }
+        case 'R': {  // topCnt
+          switch (chr) {
+            case 'L': {
+              ans += topCnt + 1;
+              stk.pop();
+              stk.push(ChrCnt{'S', 0});
+              break;
+            }
+            case 'R': {
+              stk.pop();
+              stk.push(ChrCnt{'R', topCnt + 1});
+              break;
+            }
+            case 'S': {
+              ans += topCnt;
+              stk.pop();
+              stk.push(ChrCnt{'S', 0});
+              break;
+            }
+          }
+          break;
+        }
+        case 'S': {  // topCnt
+          switch (chr) {
+            case 'L': {
+              ans += 1;
+              break;
+            }
+            case 'R': {
+              stk.push(ChrCnt{'R', 1});
+              break;
+            }
+            case 'S': {
+              break;
+            }
+          }
+          break;
+        }
       }
     }
     return ans;
