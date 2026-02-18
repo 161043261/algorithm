860bd3ca60f88ae2101735acafefc1bf698d9c45
tianchenghang <161043261@qq.com>
2026-02-19 02:34:13 +0800

    feat: Introduce new features

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
+import os
+import sys
+import re
 from typing import List
 
-
-class Solution:
-    def minPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        ans = nums[0] + nums[-1]
-        for i in range(0, len(nums) // 2):
-            ans = max(ans, nums[i] + nums[-i - 1])
-        return ans
+src_dir = sys.argv[1]
+results: List[str] = []
+for root, dirs, files in os.walk(src_dir):
+    for f in files:
+        if any(f.endswith(ext) for ext in ['.cc', '.cpp', '.cxx', '.c++']):
+            path = os.path.join(root, f)
+            with open(path, 'r', errors='ignore') as src:
+                if re.search(r'int\s+main\s*\(', src.read()):
+                    results.append(path)
+print(";".join(results))
